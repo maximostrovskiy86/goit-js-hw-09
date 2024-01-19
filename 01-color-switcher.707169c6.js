f075bc13bd282970d17e27c3d482a01f7b32760c
maximostrovskiy86 <86001982+maximostrovskiy86@users.noreply.github.com>
2024-01-19 15:08:25 +0000

    Deploying to gh-pages from @ maximostrovskiy86/goit-js-hw-09@acc086a5f0d6dfdfac45be31b96c3ccd05a60ae5 🚀

---
+++ 01-color-switcher.707169c6.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodySelector:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{t.bodySelector.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.setAttribute("disabled","true")})),t.stopBtn.addEventListener("click",(()=>{clearTimeout(e),t.startBtn.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.707169c6.js.map
