0644d0f1e7df30266d3e4f4163bb0a06b50c40da
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2024-01-26 21:57:22 +0100

    develop-future-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,40 +14,45 @@ const eventFormFunction = (event) => {
     amount: Number(target[name="amount"].value),
   };
 
+  setIntervalEvent(valuesForm)
+}
 
+function setIntervalEvent({step, delay, amount}) {
+  setTimeout(() => {
+  let count = 0;
 
-  console.log("valuesForm", valuesForm);
-  let timerId = null;
+  let timerId = setInterval(() => {
+    count += 1;
+    createPromise(count,  delay);
 
+    if (count === amount) {
+      clearInterval(timerId);
+    }
+  }, step);
 
-  for (let i = 1;  i <= valuesForm.amount; i += 1) {
-    timerId = setInterval(() => {
-      createPromise(i,  valuesForm.step);
-      console.log("valuesForm.step", valuesForm.step)
-    }, valuesForm.step)
-  }
-
-  // clearInterval(timerId);
+  }, delay);
 }
 
 function createPromise(position, delay) {
   console.log("position", position)
-  const shouldResolve = Math.random() > 0.3;
+  const shouldResolve = Math.random() > 0.45;
 
-  const promise = new Promise((resolve, reject) => {
+ return new Promise((resolve, reject) => {
+   console.log("shouldResolve", shouldResolve)
+   if (shouldResolve) {
+     // Fulfill
+      resolve((resolve) => {
+        console.log(resolve)
+      })
+     // console.log("PROMISE", promise)
 
+   } else {
+     // Reject
+     reject(reject => {
+       console.log(reject)
+     })
+   }
   })
-
-  if (shouldResolve) {
-    // Fulfill
-    promise.then((value) => {
-      console.log( "value", value)
-    })
-    // console.log("PROMISE", promise)
-
-  } else {
-    // Reject
-  }
 }
 
 refs.form.addEventListener("submit", eventFormFunction);
